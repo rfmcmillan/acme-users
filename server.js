92992d8408d4ca0c1f5f764d88282c255305c1b3
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-02-18 21:09:48 -0500

    added middleware

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const app = express();
 const path = require('path');
 const faker = require('faker');
 
-app.get('/', (req, res, next) => {res.sendFile(path.join(__dirname, 'index.html'))});
+console.log(faker.name.firstName());
+console.log(faker.name.firstName());
+
+app.use('/src', express.static(path.join(__dirname, 'src')));
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 const PORT = 3000;
 
---
+++ src/index.js
@@ -0,0 +1,4 @@
+console.log(faker.name.lastName());
+console.log(faker.name.firstName());
+console.log(faker.name.jobType());
+console.log(faker.name.jobType());
